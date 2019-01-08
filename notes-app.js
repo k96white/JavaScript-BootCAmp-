19b9551389b567e3ea7726d836a5e0a1885319ac
sanketg111 <42960017+sanketg111@users.noreply.github.com>
2019-01-08 23:01:12 +0530

    Add files via upload
    Assignment Complete till Lecture 79 Completed Section 8
    Moving Ahead with Section 9 :- Expanding your JavaScript Knowledge

--- notes-app.js
+++ notes-app.js
@@ -1,8 +1,11 @@
 let notes = getSavedNotes()
 
 const filters = {
-    searchText: ''
+    searchText: '',
+    sortBy: 'byEdited'
 }
+
+
 
 renderNotes(notes, filters)
 
@@ -29,6 +32,8 @@ document.querySelector('#search-text').addEventListener('input', function (e) {
 
 document.querySelector('#filter-by').addEventListener('change', function (e) {
     console.log(e.target.value)
+    filters.sortBy = e.target.value
+    renderNotes(notes,filters)
 })
 
 window.addEventListener('storage', function (e) {
